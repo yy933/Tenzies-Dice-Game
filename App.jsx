d2ee1b865a0cd06491852d01ed68421e16d2a440
yy933 <116449560+yy933@users.noreply.github.com>
2026-06-29 17:00:14 +0800

    feat: add generate new dice logic and roll dice button

--- App.jsx
+++ App.jsx
@@ -1,35 +1,41 @@
-/**
- * Challenge:
- *
- * - Create a Die component that takes a `value` prop. Should
- *   render a button with that value displayed.
- * - Render 10 instances of the Die component (manually)
- *      - Provide a number between 1-6 for the value on each
- *        for now
- * - Style the <main> and <Die> components
- *   to look like they do in the slide
- *      - Hints: Create a container to hold the 10 instances
- *        of the Die component, and use CSS Grid to lay them
- *        out evenly in 2 rows of 5 columns
- *      - Use flexbox on main to center the dice container
- *        in the center of the page
- */
 import Die from "./Die";
+import { useState } from "react";
+
 export default function App() {
+  /**
+   * Challenge: Create a `Roll Dice` button that will re-roll
+   * all 10 dice
+   *
+   * Clicking the button should generate a new array of numbers
+   * and set the `dice` state to that new array (thus re-rendering
+   * the array to the page)
+   */
+
+  function generateAllNewDice() {
+    let randomNumbers = Array.from({ length: 10 }, (_, i) => {
+      return Math.floor(Math.random() * 6 + 1);
+    });
+    return randomNumbers;
+  }
+
+  const [dice, setDice] = useState(generateAllNewDice());
+  function rollDice(){
+    setDice(generateAllNewDice());
+  }
+
   return (
     <main>
       <div className="dice-container">
-        <Die value={1} />
-        <Die value={2} />
-        <Die value={3} />
-        <Die value={4} />
-        <Die value={5} />
-        <Die value={6} />
-        <Die value={1} />
-        <Die value={2} />
-        <Die value={3} />
-        <Die value={4} />
+        {dice.map((value, index) => (
+          <Die key={index} value={value} />
+        ))}
       </div>
+      <button
+        className="roll-dice"
+        onClick={rollDice}
+      >
+        Roll Dice
+      </button>
     </main>
   );
 }
