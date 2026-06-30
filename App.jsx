abb33dbda648cae8e82546de00ff5d35099a6811
yy933 <116449560+yy933@users.noreply.github.com>
2026-06-30 12:52:33 +0800

    feat: add title and instructions and their styles

--- App.jsx
+++ App.jsx
@@ -3,8 +3,6 @@ import { useState } from "react";
 import { nanoid } from "nanoid";
 
 export default function App() {
-
-
   function generateAllNewDice() {
     let randomNumbers = Array.from({ length: 10 }, (_, i) => {
       return {
@@ -18,11 +16,9 @@ export default function App() {
 
   const [dice, setDice] = useState(generateAllNewDice());
   function rollDice() {
-    setDice(prevDice =>
-      prevDice.map(die =>
-        die.isHeld
-          ? die
-          : { ...die, value: Math.floor(Math.random() * 6 + 1) },
+    setDice((prevDice) =>
+      prevDice.map((die) =>
+        die.isHeld ? die : { ...die, value: Math.floor(Math.random() * 6 + 1) },
       ),
     );
   }
@@ -37,6 +33,11 @@ export default function App() {
 
   return (
     <main>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
       <div className="dice-container">
         {dice.map((die) => (
           <Die
